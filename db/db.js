598e8a45b1f7891a5b83d2dff19a493cb89a6d57
abdullah01 <arfathossen541@gmail.com>
2023-12-09 19:46:12 +0600

    install mongoose cors jwt express etc packages .Basic setup server side

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.rjnekog.mongodb.net/text?retryWrites=true&w=majority`;
+  const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.rjnekog.mongodb.net/VegetablesShopBD?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(MONGO_URI);
     console.log(`Connected to MongoDD database`);
